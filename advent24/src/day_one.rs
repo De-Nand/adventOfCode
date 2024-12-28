b2c80a88c38a82d10f7509e3c23c55bcfd298bbe
De-Nand <61670552+De-Nand@users.noreply.github.com>
2024-12-28 01:07:09 -0500

    advent 24 setup pt2

--- advent24/src/day_one.rs
+++ advent24/src/day_one.rs
@@ -21,15 +21,22 @@ pub fn calculate_result(file: File, debug: bool) -> u64 {
     let mut left_vec: Vec<u32> = Vec::new();
     let mut right_vec: Vec<u32> = Vec::new();
     for line in file_lines.flatten() {
-        if line.len() > 0{
+        if line.len() > 0 {
             let vec: Vec<&str> = line.split(' ').collect();
             left_vec.push(vec[0].parse().expect("failed to get number for left"));
             right_vec.push(vec[1].parse().expect("failed to get number for right"));
         }
     }
     // step two, loop through the list size, adding up the differences. have to account for negative.
-
-    left_vec.iter
+    for location_id in 0..left_vec.len() {
+        if left_vec[location_id] > right_vec[location_id] {
+            final_result += (left_vec[location_id] - right_vec[location_id]);
+        } else {
+            final_result += (right_vec[location_id] - left_vec[location_id]);
+        }
+    }
 
     return final_result;
 }
+
+fn add_to_list(next_entry: str, list: Vec<u32>) {}
